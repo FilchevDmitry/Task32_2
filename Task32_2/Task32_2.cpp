0d16978010b4aa7853ae09524d03688439e9f50d
Dmitry <filchev_dmitry@mail.ru>
2023-05-07 19:37:24 +0300

    Добавьте файлы проекта.

---
+++ Task32_2/Task32_2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include<string>
+#include<fstream>
+#include"nlohmann/json.hpp"
+
+int main()
+{
+	std::ifstream file("C:/Skilbox/Task32_2/Task32_2/film.json");
+	if (!file.is_open())
+	{
+		std::cout << "not";
+	}
+	else 
+	{
+		nlohmann::json o;
+		file >> o;
+		std::string name;
+
+		std::getline(std::cin , name);
+		for (auto& el : o.items()) 
+		{
+			if (o[el.key()]["Actors"] == name)
+			{
+				std::cout << el.key() << std::endl;
+			}			
+		}
+	}
+	return 0;
+}
